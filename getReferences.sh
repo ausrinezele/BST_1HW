c0eb84b1af520bbc8590c6522b48c95627bcd7dd
Ausrine Zeleckyte <ausrine.zele@gmail.com>
2023-03-07 16:01:04 +0200

    update

--- getReferences.sh
+++ getReferences.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 # Download Genome reference FASTA
-wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/dna/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz" 
+wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculusls/dna/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz" 
 gzip -d Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz
 
 # Download transcriptome reference FASTA.gz
-wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/dna/Mus_musculus.NCBIM37.67.cdna.all.fa.gz" 
+wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/cdna/Mus_musculus.NCBIM37.67.cdna.all.fa.gz" 
 gzip -d Mus_musculus.NCBIM37.67.cdna.all.fa.gz
 
 # Download gtf file
-wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/dna/Mus_musculus.NCBIM37.67.gtf.gz"
+wget "https://ftp.ensembl.org/pub/release-67/gtf/mus_musculus/Mus_musculus.NCBIM37.67.gtf.gz"
 gzip -d Mus_musculus.NCBIM37.67.gtf.gz
 
-fastq-dump --outdir /inputs --gzip --skip-technical --readids --dumpbase --split-files SRR8985047 SRR8985048 SRR8985051 SRR8985052
+# Download raw FASTQ files
+prefetch SRR8985047 SRR8985048 SRR8985051 SRR8985052
+fastq-dump --gzip SRR8985047 SRR8985048 SRR8985051 SRR8985052
+
 
  
