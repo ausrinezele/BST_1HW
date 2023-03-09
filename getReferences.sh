aec4cd888d7b86468c5002ccb43c5c2e7a2c75b1
ausrinezele <ausrine.zele@gmail.com>
2023-03-09 16:48:17 +0200

    sutvarkytos direkcijos

--- getReferences.sh
+++ getReferences.sh
@@ -2,19 +2,20 @@
 
 # Download Genome reference FASTA
 wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculusls/dna/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz" 
-gzip -d Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz
+gzip -d ../../references/Mus_musculus.NCBIM37.67.dna.toplevel.fa.gz
 
 # Download transcriptome reference FASTA.gz
 wget "https://ftp.ensembl.org/pub/release-67/fasta/mus_musculus/cdna/Mus_musculus.NCBIM37.67.cdna.all.fa.gz" 
-gzip -d Mus_musculus.NCBIM37.67.cdna.all.fa.gz
+gzip -d ../../references/Mus_musculus.NCBIM37.67.cdna.all.fa.gz
 
 # Download gtf file
 wget "https://ftp.ensembl.org/pub/release-67/gtf/mus_musculus/Mus_musculus.NCBIM37.67.gtf.gz"
-gzip -d Mus_musculus.NCBIM37.67.gtf.gz
+gzip -d ../../references/Mus_musculus.NCBIM37.67.gtf.gz
 
 # Download raw FASTQ files
-prefetch SRR8985047 SRR8985048 SRR8985051 SRR8985052
-fastq-dump --gzip SRR8985047 SRR8985048 SRR8985051 SRR8985052
+prefetch ../../inputs/SRR8985047 ../../inputs/SRR8985048 ../../inputs/SRR8985051 ../../inputs/SRR8985052
+fastq-dump --gzip ../../inputs/SRR8985047 ../../inputs/SRR8985048 ../../inputs/SRR8985051 ../../inputs/SRR8985052
 
+grep '>' Mus_musculus.NCBIM37.67.cdna.all.fa | cut -d ' ' -f1 | sort -u | wc -l
 
  
