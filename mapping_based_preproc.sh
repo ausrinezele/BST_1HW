eb2e1af6adac86a60471c911e15bf599657b3b42
ausrinezele <ausrine.zele@gmail.com>
2023-03-22 11:14:34 +0200

    pilnai klasikinis variantas

--- mapping_based_preproc.sh
+++ mapping_based_preproc.sh
@@ -4,7 +4,7 @@ threads=6
 
 # Path to reference genome FASTA file
 ref_genome="../../references/Mus_musculus.NCBIM37.67.dna.toplevel.fa"
-
+ref_genome_gtf="../../references/Mus_musculus.NCBIM37.67.gtf"
 # check if HISAT2 index files exist for reference genome
 if [ ! -f ${ref_genome}.1.ht2 ]; then
   echo "HISAT2 index files not found. Generating index..."
@@ -33,7 +33,7 @@ trim_galore -j ${threads} --length 20 -o ../../outputs/trimmed --paired  ../../i
 trim_galore -j ${threads} --length 20 -o ../../outputs/trimmed --paired  ../../inputs/SRR8985052_1.fastq.gz  ../../inputs/SRR8985052_2.fastq.gz
 
 # Rerun FASTQC on newly created/cleaned FASTQ files
-for i in ../../outputs/trimmed/*_1_val_1.fq.gz; 
+for i in ../../outputs/trimmed/*_1_val_1.fq.gz 
 do 
     R1=${i}
     R2="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_2_val_2.fq.gz"
@@ -43,3 +43,44 @@ done
 
 # Create MultiQC plots for raw and processed data
 multiqc ../../outputs -o ../../outputs/multiq_processed
+
+# Mapped each sample to the reference genome. Removed unmapped reads as well as reads that are mapped incorrectly. Mapping results in BAM format
+# Deduplicate files and index BAM files.
+for i in ../../outputs/trimmed/*_1_val_1.fq.gz
+do
+    R1=${i}
+    R2="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_2_val_2.fq.gz"
+    SAM="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)".sam"
+    BAM="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)".bam"
+    BAM_S="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_sorted.bam"
+    BAM_Fil="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_filtered.bam"
+    BAM_F="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_fixmate.bam"
+    BAM_P="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_positionsort.bam"
+    BAM_M="../../outputs/trimmed/"$(basename ${i} _1_val_1.fq.gz)"_markdup.bam"
+    hisat2 -p ${threads} --dta -x ${ref_genome} -1 ${R1} -2 ${R2} -S ${SAM}
+    samtools view -@ 6 -bS ${SAM} -o ${BAM}
+    samtools view -@ 6 -F 260 -bS ${BAM} > ${BAM_Fil}
+    samtools sort -n -o ${BAM_S} ${BAM_Fil}
+    samtools fixmate -m ${BAM_S} ${BAM_F}
+    samtools sort -o ${BAM_P} ${BAM_F}
+    samtools markdup -r ${BAM_P} ${BAM_M}
+    samtools index ${BAM_M}
+done
+
+# Stringtie quantification on BAM files.
+for i in ../../outputs/trimmed/*_markdup.bam 
+do
+    GTF="../../outputs/stringtie/"$(basename ${i} _markdup.bam)".gtf"
+    BAM_M="../../outputs/trimmed/"$(basename "${i}" .markdup.bam)
+    prefix=$(basename ${i} _markdup.bam)
+    stringtie -p ${threads} -G ${ref_genome_gtf} -o ${GTF} ${BAM_M}
+done
+
+# Create a correlation diagram as well as a PCA plot for data
+multiBamSummary bins --outFileName ../../results/mapped.npz --binSize 1000 -p ${threads} --outRawCounts ../../results/raw_counts.tsv -b ../../outputs/trimmed/*_markdup.bam
+
+plotCorrelation -in ../../results/mapped.npz -c pearson -p heatmap -o ../../results/mapped_data_heatmap.pdf
+plotPCA -in ../../results/mapped.npz -o ../../results/mapped_data_PCA.pdf
+
+
+
